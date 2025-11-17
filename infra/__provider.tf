be72c75d01c980b7f1a540a1f78d09d3964f0ae2
Tejas Cherukara <tejas.cherukara@gmail.com>
2025-11-17 20:42:29 +1100

    Update Terraform version and providers

--- infra/__provider.tf
+++ infra/__provider.tf
@@ -1,15 +1,33 @@
 # Configure the Cloudflare provider.
-provider "cloudflare" {
-  version = "~> 2.0"
-}
+provider "cloudflare" {}
 
 provider "google" {
-  project     = "sandbox-project-tc"
-  region      = "australia-southeast1"
+  project = "sandbox-project-tc"
+  region  = "australia-southeast1"
+}
+
+provider "google-beta" {
+  project = "sandbox-project-tc"
+  region  = "australia-southeast1"
 }
 
 terraform {
-  required_version = ">= 0.12.24"
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 2.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.26.0, < 8.0.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.26.0, < 8.0.0"
+    }
+  }
 
   backend "remote" {
     hostname      = "app.terraform.io"
