8f1c3607e6c1c33361665079853ae7e80b757f67
Tejas Cherukara <tejas.cherukara@gmail.com>
2025-11-19 13:53:33 +1100

    Patch additional VM helpers for globalThis

--- scripts/globalthis-polyfill.js
+++ scripts/globalthis-polyfill.js
@@ -10,8 +10,8 @@ if (typeof globalThis === "undefined") {
 if (typeof global !== "undefined") {
   // Some Gatsby internals evaluate code within isolated VM contexts.
   const vm = require("vm");
-  const originalRunInNewContext = vm.runInNewContext;
-  vm.runInNewContext = function runInNewContext(code, context = {}, options) {
+
+  const ensureGlobalThis = context => {
     if (context && typeof context === "object" && !context.globalThis) {
       Object.defineProperty(context, "globalThis", {
         value: context,
@@ -20,6 +20,29 @@ if (typeof global !== "undefined") {
         writable: true,
       });
     }
-    return originalRunInNewContext.call(vm, code, context, options);
+    return context;
+  };
+
+  const originalRunInNewContext = vm.runInNewContext;
+  vm.runInNewContext = function runInNewContext(code, context = {}, options) {
+    return originalRunInNewContext.call(vm, code, ensureGlobalThis(context), options);
+  };
+
+  const originalRunInContext = vm.runInContext;
+  vm.runInContext = function runInContext(code, contextifiedSandbox, options) {
+    ensureGlobalThis(contextifiedSandbox);
+    return originalRunInContext.call(vm, code, contextifiedSandbox, options);
+  };
+
+  const Script = vm.Script;
+  const originalScriptRunInNewContext = Script.prototype.runInNewContext;
+  Script.prototype.runInNewContext = function(context = {}, options) {
+    return originalScriptRunInNewContext.call(this, ensureGlobalThis(context), options);
+  };
+
+  const originalScriptRunInContext = Script.prototype.runInContext;
+  Script.prototype.runInContext = function(contextifiedSandbox, options) {
+    ensureGlobalThis(contextifiedSandbox);
+    return originalScriptRunInContext.call(this, contextifiedSandbox, options);
   };
 }
