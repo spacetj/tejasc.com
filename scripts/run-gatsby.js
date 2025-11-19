d40ec9f52757ea88e979451a66068ca2f73ca447
Tejas Cherukara <tejas.cherukara@gmail.com>
2025-11-19 13:42:01 +1100

    Ensure vm contexts receive globalThis polyfill

--- scripts/run-gatsby.js
+++ scripts/run-gatsby.js
@@ -9,4 +9,18 @@ if (typeof globalThis === "undefined") {
   });
 }
 
+const vm = require("vm");
+const originalRunInNewContext = vm.runInNewContext;
+vm.runInNewContext = function runInNewContext(code, context = {}, options) {
+  if (context && typeof context === "object" && !context.globalThis) {
+    Object.defineProperty(context, "globalThis", {
+      value: context,
+      configurable: true,
+      enumerable: false,
+      writable: true,
+    });
+  }
+  return originalRunInNewContext.call(vm, code, context, options);
+};
+
 require("gatsby/dist/bin/gatsby");
