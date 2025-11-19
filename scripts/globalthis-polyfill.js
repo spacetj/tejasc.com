874f2048815b5dbe68a9099ef9f02a0d3b24970c
spacetj <tejas.cherukara@gmail.com>
2025-11-19 15:50:50 +1100

    Merge commit 60d6929 into work to retain history

---
+++ scripts/globalthis-polyfill.js
@@ -0,0 +1,57 @@
+if (typeof globalThis === "undefined") {
+  Object.defineProperty(global, "globalThis", {
+    value: global,
+    configurable: true,
+    enumerable: false,
+    writable: true,
+  });
+}
+
+if (typeof global !== "undefined") {
+  // Some Gatsby internals evaluate code within isolated VM contexts.
+  const vm = require("vm");
+  const Module = require("module");
+
+  const originalResolveFilename = Module._resolveFilename;
+  Module._resolveFilename = function patchedResolve(request, parent, isMain, options) {
+    if (typeof request === "string" && request.startsWith("node:")) {
+      request = request.slice(5);
+    }
+    return originalResolveFilename.call(this, request, parent, isMain, options);
+  };
+
+  const ensureGlobalThis = context => {
+    if (context && typeof context === "object" && !context.globalThis) {
+      Object.defineProperty(context, "globalThis", {
+        value: context,
+        configurable: true,
+        enumerable: false,
+        writable: true,
+      });
+    }
+    return context;
+  };
+
+  const originalRunInNewContext = vm.runInNewContext;
+  vm.runInNewContext = function runInNewContext(code, context = {}, options) {
+    return originalRunInNewContext.call(vm, code, ensureGlobalThis(context), options);
+  };
+
+  const originalRunInContext = vm.runInContext;
+  vm.runInContext = function runInContext(code, contextifiedSandbox, options) {
+    ensureGlobalThis(contextifiedSandbox);
+    return originalRunInContext.call(vm, code, contextifiedSandbox, options);
+  };
+
+  const Script = vm.Script;
+  const originalScriptRunInNewContext = Script.prototype.runInNewContext;
+  Script.prototype.runInNewContext = function(context = {}, options) {
+    return originalScriptRunInNewContext.call(this, ensureGlobalThis(context), options);
+  };
+
+  const originalScriptRunInContext = Script.prototype.runInContext;
+  Script.prototype.runInContext = function(contextifiedSandbox, options) {
+    ensureGlobalThis(contextifiedSandbox);
+    return originalScriptRunInContext.call(this, contextifiedSandbox, options);
+  };
+}
