f7a99b3fe15d327cd922236027f4c21ab22905a3
Tejas Cherukara <tejas.cherukara@gmail.com>
2025-11-21 17:03:50 +1100

    chore: pin google providers to 3.90 for tf0.13

--- infra/__provider.tf
+++ infra/__provider.tf
@@ -21,11 +21,11 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.26.0, < 8.0.0"
+      version = "~> 3.90"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 5.26.0, < 8.0.0"
+      version = "~> 3.90"
     }
   }
 
